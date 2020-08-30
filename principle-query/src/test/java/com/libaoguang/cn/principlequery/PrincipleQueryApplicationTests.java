0faeae2410ec399617df5f0c86d3b70fe49d6518
baoguang1993 <bigbao1993>
2020-08-30 22:08:28 +0800

    es

--- principle-query/src/test/java/com/libaoguang/cn/principlequery/PrincipleQueryApplicationTests.java
+++ principle-query/src/test/java/com/libaoguang/cn/principlequery/PrincipleQueryApplicationTests.java
@@ -1,16 +1,35 @@
 package com.libaoguang.cn.principlequery;
 
 import lombok.extern.slf4j.Slf4j;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.xcontent.XContentBuilder;
+import org.elasticsearch.common.xcontent.XContentFactory;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 
 @Slf4j
-@RunWith(SpringJUnit4ClassRunner.class)
+@RunWith(SpringRunner.class)
 @SpringBootTest(classes = PrincipleQueryApplication.class)
-class PrincipleQueryApplicationTests {
+public class PrincipleQueryApplicationTests {
+    @Autowired
+    private TransportClient client;
 
+   @Test
+    public void add()throws Exception{
+       XContentBuilder contentBuilder= XContentFactory.jsonBuilder()
+               .startObject()
+               .field("title","qqq")
+               .field("author","111")
+               .endObject();
+       IndexResponse indexResponse = this.client.prepareIndex("book", "novel")
+               .setSource(contentBuilder).get();
+       String id = indexResponse.getId();
+       System.out.println("id"+id);
 
+   }
 }
