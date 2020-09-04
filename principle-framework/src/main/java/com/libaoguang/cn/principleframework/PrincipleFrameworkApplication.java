39da8f9355863d7be4e8de97d7f31832d8daf6b6
baoguang1993 <bigbao1993>
2020-09-05 00:12:32 +0800

    es

---
+++ principle-framework/src/main/java/com/libaoguang/cn/principleframework/PrincipleFrameworkApplication.java
@@ -0,0 +1,12 @@
+package com.libaoguang.cn.principleframework;
+
+
+import lombok.Getter;
+
+public class PrincipleFrameworkApplication {
+
+    public static void main(String[] args) {
+
+    }
+
+}
